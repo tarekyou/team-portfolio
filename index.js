ee955cbdb71228b78e2e893b85be1fbe0158e4c5
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-14 20:18:03 -0800

    added require to app.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const fs = require('fs')
+const inquirer = require('inquirer')
